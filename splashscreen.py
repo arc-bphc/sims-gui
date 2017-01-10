3a6e8cd5a77016da75980a0c920cb44759786d73
arnav dhamija <arnav.dhamija@gmail.com>
2017-01-10 16:46:22 +0530

    Renamed files

--- splashscreen.py
+++ splashscreen.py
@@ -1,7 +1,6 @@
-#!/usr/local/bin/python
-import sys
+#import sys
 from PyQt4 import QtCore, QtGui
-from mainwindow import Ui_splashScreen
+from ui_mainwindow import Ui_splashScreen
 
 class splashScreen(Ui_splashScreen):
     def __init__(self, window):
@@ -10,10 +9,3 @@ class splashScreen(Ui_splashScreen):
         self.pushButton.clicked.connect(self.launchWindow)
     def launchWindow(self):
         print "hello"
-
-if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
-    window = QtGui.QMainWindow()
-    prog = splashScreen(window)
-    window.show()
-    sys.exit(app.exec_())
