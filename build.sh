0d961072131fc3fbaaedc3877d0d53800e616911
arnav dhamija <arnav.dhamija@gmail.com>
2017-01-26 15:44:22 +0530

    Added new ui codes

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ pyuic4 finger.ui -o ui_finger.py
 pyuic4 inventory.ui -o ui_inventory.py
 pyuic4 editdetails.ui -o ui_editdetails.py
 pyuic4 userprofile.ui -o ui_userprofile.py
+pyuic4 resetpin.ui -o ui_resetpin.py
+pyuic4 requestitem.ui -o ui_requestitem.ui
