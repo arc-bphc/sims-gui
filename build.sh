d594d1d97d9c7ad27d038255f3c815fd8664cb62
arnav dhamija <arnav.dhamija@gmail.com>
2017-02-10 16:40:04 +0530

    Added more files

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ pyuic4 inventory.ui -o ui_inventory.py
 pyuic4 editdetails.ui -o ui_editdetails.py
 pyuic4 userprofile.ui -o ui_userprofile.py
 pyuic4 resetpin.ui -o ui_resetpin.py
-pyuic4 requestitem.ui -o ui_requestitem.ui
+pyuic4 requestitem.ui -o ui_requestitem.py
--- userprofile.py
+++ userprofile.py
@@ -2,6 +2,8 @@ from PyQt4 import QtCore, QtGui
 from ui_userprofile import Ui_userWindow
 from inventorywindow import inventoryWindow
 from editdetailswindow import editDetailsWindow
+from requestitem import requestItemWindow
+
 class userProfile(Ui_userWindow):
     def __init__(self, widget):
         Ui_userWindow.__init__(self)
@@ -9,6 +11,8 @@ class userProfile(Ui_userWindow):
 
         self.inventoryButton.clicked.connect(lambda: self.launchInventory(widget))
         self.editDetailsButton.clicked.connect(lambda: self.launchEditDetails(widget))
+        self.requestButton.clicked.connect(lambda: self.launchRequestItem(widget))
+        self.resetPinButton.clicked.connect(lambda: self.launchResetForm(widget))
 
         self.winddowWidget = QtGui.QMainWindow()
         self.childWidget = QtGui.QWidget()
@@ -21,3 +25,8 @@ class userProfile(Ui_userWindow):
         prog = editDetailsWindow(self.childWidget)
         self.childWidget.showFullScreen()
         widget.close()
+    def launchRequestItem(self, widget):
+        prog = requestItemWindow(self.childWidget)
+        self.childWidget.showFullScreen()
+        widget.close()
+#    def launchResetForm(self, widget)
