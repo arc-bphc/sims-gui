5d89bb463a0f0d0204c504c60c137b3108168763
arnav dhamija <arnav.dhamija@gmail.com>
2017-02-10 23:08:31 +0530

    Deleted all the useless files

--- editdetailswindow.py
+++
@@ -1,7 +0,0 @@
-from PyQt4 import QtCore, QtGui
-from ui_editdetails import Ui_editDetailsWindow
-
-class editDetailsWindow(Ui_editDetailsWindow):
-    def __init__(self, widget):
-        Ui_editDetailsWindow.__init__(self)
-        self.setupUi(widget)
--- inventorywindow.py
+++
@@ -1,7 +0,0 @@
-from PyQt4 import QtCore, QtGui
-from ui_inventory import Ui_inventoryWindow
-
-class inventoryWindow(Ui_inventoryWindow):
-    def __init__(self, widget):
-        Ui_inventoryWindow.__init__(self)
-        self.setupUi(widget)
