e8e10481aadca1b78e3e85d2a54cb8859f578ce3
arnav dhamija <arnav.dhamija@gmail.com>
2017-02-10 20:50:09 +0530

    Making good progress now

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 from PyQt4 import QtCore, QtGui
 import sys
 from ui_mainwindow import Ui_splashScreen
-from ui_resetpin import Ui_resetPinWindow
+from ui_userprofile import Ui_userWindow
 
 class mainWindow(QtGui.QWidget):
     def __init__(self, widget):
         super(mainWindow, self).__init__()
-        self.stack1 = QtGui.QWidget()
-        self.x = Ui_resetPinWindow()
-        self.x.setupUi(self.stack1)
+
+        self.splashScreen = QtGui.QMainWindow()
+        self.userProfile = QtGui.QDialog()
+        self.resetPin = QtGui.QWidget()
+        self.fingerprint = QtGui.QDialog()
+        self.requestItem = QtGui.QWidget()
+        self.editDetails = QtGui.QWidget()
+        self.inventory = QtGui.QMainWindow()
+
+        self.StackWidget = QtGui.QStackedWidget(self)
+        self.createStackedPages()
+
         hbox = QtGui.QHBoxLayout()
-        hbox.addWidget(self.stack1)
-        #hbox.addWidget(QtGui.QRadioButton("Male"))
+        hbox.addWidget(self.StackWidget)
         widget.setLayout(hbox)
-        print "done"
+
+        self.StackWidget.setCurrentIndex(0)
+
+    def printString(self):
+        print "HellowORld"
+
+    def createStackedPages(self):
+        Ui_userWindow().setupUi(self.userProfile)
+
+        #f = self.splashScreen.gridLayout
+        #self.pushButton.clicked.connect(lambda: self.printString())
+        self.StackWidget.addWidget(self.splashScreen)
+        self.StackWidget.addWidget(self.userProfile)
+        self.StackWidget.addWidget(self.resetPin)
+        self.StackWidget.addWidget(self.fingerprint)
+        self.StackWidget.addWidget(self.requestItem)
+        self.StackWidget.addWidget(self.editDetails)
+        self.StackWidget.addWidget(self.inventory)
+
+    def setupSplashScreen(self):
+        Ui_splashScreen().setupUi(self.splashScreen)
+        button = self.splashScreen.findChild(QtGui.QPushButton, "pushButton")
+        button.clicked.connect(lambda: self.launchUserProfile())
+
+    def launchUserProfile(self):
+        self.StackWidget.setCurrentIndex(1)
 
 def main():
     app = QtGui.QApplication(sys.argv)
