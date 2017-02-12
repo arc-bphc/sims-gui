da523030363e6c62684592a824fd2b5c6ba88c1e
yashdeep97 <yashdeep97@gmail.com>
2017-02-13 04:52:28 +0530

    created library to return user_info list

--- sql/user_info.py
+++ sql/user_info.py
@@ -1,7 +1,18 @@
-from insert_data_users.py import *
+from insert_data_users import *
 
 class user_info:
 
-	def get_user_info(id):
-		user = db('test.db')
-		user_info_list = user.selectQuery('users',['*'],['ID = ' + id])
+	def get_user_info(self, id):
+		self.user = db('test.db')
+		user_info_list = self.user.selectQuery('users',['*'],['ID = ' + str(id)])
+		return user_info_list
+
+
+def main():
+	obj = user_info()
+	user_info_list = obj.get_user_info(1)
+	print user_info_list
+
+if __name__ == '__main__':
+    main()
+    
