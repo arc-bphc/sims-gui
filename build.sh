683ceedf286247d5433fbadb029a9821dbabd518
arnav dhamija <arnav.dhamija@gmail.com>
2017-02-15 19:24:47 +0530

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-pyuic4 mainwindow.ui -o ui_mainwindow.py
+pyuic4 mainwindow.ui -o ui_mainwindow.pyrick ast
 pyuic4 finger.ui -o ui_finger.py
 pyuic4 inventory.ui -o ui_inventory.py
 pyuic4 editdetails.ui -o ui_editdetails.py
