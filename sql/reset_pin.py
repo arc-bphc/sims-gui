d3a0bca9afa4998d98d5c9d0cf003bcd14267ffe
yashdeep97 <yashdeep97@gmail.com>
2017-02-16 02:33:31 +0530

    modified reset_pin.py, and able to compare pin correctly now

--- sql/reset_pin.py
+++ sql/reset_pin.py
@@ -6,26 +6,30 @@ class resetPin:
 	def __init__(self):
 		self.user = db('test.db')
 
-	def comparePin(self, id, pin):
-		print pin
-		user_list = self.user.selectQuery('users',['*'],['ID = ' + str(id)])
-		salt = user_list[0][6]
-		print salt
-		hashed_pin = user_list[0][7]
-		print hashed_pin
-		pin = pin + salt
-		print pin
-    	entered_pin = SHA256.new(pin).hexdigest()
-    	print entered_pin
-    	# if hashed_pin == entered_pin:
-    	# 	print 1
-    	# else:
-    	# 	print 0
+	def compareEnteredPin(self,id,pin):
+		return comparePin(self,id,pin)
+
+
+def comparePin(obj,id, pin):
+	print pin
+	user_list = obj.user.selectQuery('users',['*'],['ID = ' + str(id)])
+	salt = user_list[0][6]
+	# print salt
+	hashed_pin = user_list[0][7]
+	print "hashed_pin from database:  " + hashed_pin
+	pin = pin + salt
+	# print pin
+	entered_pin = SHA256.new(pin).hexdigest()
+	print "entered_pin:	" + entered_pin
+	if hashed_pin == entered_pin:
+		return 1
+	else:
+		return 0
 
 
 def main():
 	obj = resetPin()
-	obj.comparePin(1,'1234')	
+	print obj.compareEnteredPin(1,'1234')	
 
 if __name__ == '__main__':
     main()
