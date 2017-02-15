410cf368e462c948cf4de8e9d7cdaff6a47cdc54
yashdeep97 <yashdeep97@gmail.com>
2017-02-16 00:36:48 +0530

    fixed view_cart.py

--- sql/view_cart.py
+++ sql/view_cart.py
@@ -1,30 +1,38 @@
 from insert_data_users import *
 
+item_list = []
+items_issued = []
+quantity = []
+final_list = []
+
 class view_cart:
-	item_list,quantity
 
 	def __init__(self):
 		self.user = db('test.db')
 
 	def getItemList(self, id):
 		item_list = self.user.selectQuery('transactions',['*'],['ID = ' + str(id)])
-		for i in item_list:
-			items_issued.insert(i,item_list[i][2])
+		# print item_list
+		for i in range(len(item_list)):
+			items_issued.append(item_list[i][2])
 			quantity.insert(i,item_list[i][3])
-		print items_issued
-		for j in items_issued:
-			self.item_info_list.insert(j,self.user.selectQuery('inventory',['*'],['ITEM_ID = ' + str(items_issued[j])]))
+		# print items_issued
+		# print quantity
+		for j in range(len(items_issued)):
+			self.item_info_list = self.user.selectQuery('inventory',['*'],['ITEM_ID = ' + str(items_issued[j])])
 		return items_issued
 
 	def getItemInfo(self,item_no):
-		final_list = [self.item_info_list[item_no][0],self.item_info_list[item_no][1],self.item_info_list[item_no][2],self.item_info_list[item_no][3],self.item_info_list[item_no][4],self.item_info_list[item_no][5],self.quantity[item_no]]
+		# print self.item_info_list
+		final_list = [self.item_info_list[item_no][0],self.item_info_list[item_no][1],self.item_info_list[item_no][2],self.item_info_list[item_no][3],self.item_info_list[item_no][4],self.item_info_list[item_no][5],quantity[item_no]]
+		# print final_list
 		return final_list
 
 def main():
 	obj = view_cart()
-	print obj.getItemList(1)
+	obj.getItemList(1)
 	print '\n'
-	print obj.getItemInfo(1)	
+	obj.getItemInfo(0)	
 
 if __name__ == '__main__':
     main()
