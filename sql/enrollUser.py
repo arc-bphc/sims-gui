568c4ac469c1e4aae0b52d5ea209a046cb263568
yashdeep97 <yashdeep97@gmail.com>
2017-06-22 13:05:09 +0530

    modified insert user query for 2 extra fields in the database

--- sql/enrollUser.py
+++ sql/enrollUser.py
@@ -4,9 +4,9 @@ class enrollUser:
         def __init__(self,dbname):
                 self.user = db(dbname)
 
-        def enrollNewUser(self,name,emailID,phoneCall,phoneWhatsapp,roomNo,pin,fingerID,isAdmin):
+        def enrollNewUser(self,name,emailID,phoneCall,phoneWhatsapp,roomNo,pin,fingerID,isAdmin,doorAccess,inventoryAccess):
                 password = self.createPassword(pin)
-                self.user.insertTuple('users', [name, emailID, phoneCall, phoneWhatsapp, roomNo, password['salt'], password['hash'], fingerID, isAdmin],['NAME','EMAIL_ID','PHONE_CALL','PHONE_WHATSAPP','ROOM_NO','SALT','HASHED_PASSWORD','FINGERPRINT_ID','ISADMIN'])
+                self.user.insertTuple('users', [name, emailID, phoneCall, phoneWhatsapp, roomNo, password['salt'], password['hash'], fingerID, isAdmin, doorAccess, inventoryAccess],['NAME','EMAIL_ID','PHONE_CALL','PHONE_WHATSAPP','ROOM_NO','SALT','HASHED_PASSWORD','FINGERPRINT_ID','ISADMIN','DOOR_ACCESS','INVENTORY_ACCESS'])
         def createPassword(self,text):
                 password = {}
                 #changed the salt algo to be ascii
@@ -20,9 +20,9 @@ class enrollUser:
                 password['hash'] = hash_object.hexdigest()
                 return password
 
-def main():
-	obj = enrollUser('test.db')
-	obj.enrollNewUser('yashdeep','yashdeep97@gmail.com','9010712068','9665333384','bm036','1234',123,1)
+# def main():
+# 	obj = enrollUser('test.db')
+# 	obj.enrollNewUser('yashdeep','yashdeep97@gmail.com','9010712068','9665333384','bm036','1234',123,1)
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
